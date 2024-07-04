1bc62645d8d4672f0208df87a21d46b3e4840550
heritsilavo <heritsilavo4835@gmail.com>
2024-07-04 19:33:27 +0300

    tryMongodbAdapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,30 +1,29 @@
-// /pages/api/auth/[...nextauth].js
-
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
 import GoogleProvider from 'next-auth/providers/google';
 import GithubProvider from 'next-auth/providers/github';
-
+import clientPromise from "@/lib/mongodb"
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string
     }),
     GithubProvider({
-        clientId:process.env.GITHUB_ID as string,
-        clientSecret:process.env.GITHUB_SECRET as string,
+        clientId:process.env.NEXT_PUBLIC_GITHUB_ID as string,
+        clientSecret:process.env.NEXT_PUBLIC_GITHUB_SECRET as string,
         httpOptions:{
             timeout:10000
         }
     })
     // Ajoutez d'autres fournisseurs ici
   ],
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXT_PUBLIC_SECRET,
   callbacks: {
     async session({ session, user, token }) {
       return {id:user.id,...session};
